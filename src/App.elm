e2f85940920d587fa784469cc2b02bad0e1c38f1
Pol Gómez Riquelme <polgomezriquelme@gmail.com>
2020-05-23 21:27:25 +0200

    Load file

--- src/App.elm
+++ src/App.elm
@@ -4,24 +4,55 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Browser
+import File exposing (File)
+import File.Select as Select
+import Task
 
+main : Program Flags Model Msg
 main = 
-  Browser.sandbox { init = initModel, view = view, update = update }
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
 type alias Model = 
-  { machine : Chip16 }
+  { machine : Chip16
+  , file : Maybe String
+  }
 
-type Msg = LoadExecPrompt
+type Msg
+  = FileRequested
+  | FileLoaded File
+  | FileContentLoaded String
+
+type alias Flags = ()
 
 initModel : Model
 initModel =
-  { machine = Chip16.init }
+  { machine = Chip16.init
+  , file = Nothing
+  }
+
+init : Flags -> (Model, Cmd Msg)
+init () =
+    (initModel, Cmd.none)
 
 view : Model -> Html Msg
 view model =
   div []
-    [ text ("PC=" ++ Debug.toString model.machine.cpu.pc),
-      button [ onClick LoadExecPrompt ] [ text "file" ] ]
+    [ text ("File = " ++ Debug.toString model.file),
+      br [] [],
+      button [ class "btn btn-primary", onClick FileRequested ] [ text "Load ROM" ] ]
 
-update : Msg -> Model -> Model
-update msg model = model
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    FileRequested -> (model, Select.file ["application/octet-stream"] FileLoaded)
+    FileLoaded f -> (model, Task.perform FileContentLoaded (File.toString f))
+    FileContentLoaded s -> ({ model | file = Just s }, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
